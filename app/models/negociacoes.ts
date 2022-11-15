e048cef6e2907d0ef88cd331a73bc081b8df8920
Bruno <bruno.bragaramos@gmail.com>
2022-11-14 22:44:34 -0300

    update

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,5 +1,19 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
- private negociacoes: Array<Negociacao> = []; 
+   private negociacoes: Array<Negociacao> = []; 
+
+   //RECEBE A NEGOCIÇÃO E ADICIONA NO ARRAY NEGOCIÇÕES
+   //RECEBE COMO PARAMETRO 'negociacao' DO TIPO 'Negociacao', POIS É O ÚNICO TIPO DE OBEJETO QUE A LISTA DE NEGOCIÇÕES ACEITA
+   //'push' -> FUNÇÃO QUE LIGA O ARRAY AOS OBJETOS QUE DEVE RECEBER   
+   adiciona(negociacao: Negociacao) {
+    this.negociacoes.push(negociacao);
+   }
+   
+   // RETONAR A LISTA DE NEGOCIAÇÕES E TUDO O QUE FOI ADICIONADO NELA
+   // 'ReadonlyArray' -> SOMENTE LEITURA, OU SEJA, IMPOSSÍVEL USAR METODOS QUE ALTEREM O ARRAY
+   lista(): ReadonlyArray<Negociacao> {
+    return this.negociacoes;
+   }
+
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,4 +2,15 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    //RECEBE A NEGOCIÇÃO E ADICIONA NO ARRAY NEGOCIÇÕES
+    //RECEBE COMO PARAMETRO 'negociacao' DO TIPO 'Negociacao', POIS É O ÚNICO TIPO DE OBEJETO QUE A LISTA DE NEGOCIÇÕES ACEITA
+    //'push' -> FUNÇÃO QUE LIGA O ARRAY AOS OBJETOS QUE DEVE RECEBER   
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    // RETONAR A LISTA DE NEGOCIAÇÕES E TUDO O QUE FOI ADICIONADO NELA
+    // 'ReadonlyArray' -> SOMENTE LEITURA, OU SEJA, IMPOSSÍVEL USAR METODOS QUE ALTEREM O ARRAY
+    lista() {
+        return this.negociacoes;
+    }
 }
